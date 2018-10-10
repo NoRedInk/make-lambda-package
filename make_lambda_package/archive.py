208de4b9203e5655d57ef5af4267ed5f92d6470f
Fredric Vollmer <fredricvollmer@Fredrics-iMac.local>
2018-10-09 21:27:47 -0600

    Set executable perms on files added to zip package

--- make_lambda_package/archive.py
+++ make_lambda_package/archive.py
@@ -1,6 +1,7 @@
 import os.path
 import glob
-from zipfile import ZipFile, ZIP_DEFLATED
+import time
+from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED
 
 from make_lambda_package import fsutil
 
@@ -23,14 +24,14 @@ def make_archive(
 
 def _add_local_files(zipfile, local_source_files):
     for (source, dest) in local_source_files:
-        zipfile.write(source, dest)
+        _add_file_to_zip(zipfile, source, dest)
 
 
 def _add_repo_files(zipfile, paths, repo_source_files):
     with fsutil.chdir(paths.src_dir):
         for glob_pattern in repo_source_files:
             for path in glob.glob(glob_pattern):
-                zipfile.write(path)
+                _add_file_to_zip(zipfile, path)
 
 
 def _add_deps(zipfile, paths, deps_file, python):
@@ -41,3 +42,18 @@ def _add_deps(zipfile, paths, deps_file, python):
             if line.startswith(os.pardir):
                 continue
             zipfile.write(line.strip())
+
+
+def _add_file_to_zip(zipfile, path, archive_dest=None):
+    with open(path, 'r') as f:
+        file_bytes = f.read()
+        info = ZipInfo(path)
+        info.date_time = time.localtime()
+        # Set permissions to be executable
+        info.external_attr = 0o100755 << 16
+
+        # If archive dest was provided, use that as path
+        if archive_dest:
+            info.filename = archive_dest
+
+        zipfile.writestr(info, file_bytes, ZIP_DEFLATED)
